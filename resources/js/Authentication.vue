e188cddf43c6dea7dfc499fda7d2bf9ae342cc7f
Roland Németh <info@nemethroland.hu>
2023-04-27 09:08:30 +0200

    Auto format

--- resources/js/Authentication.vue
+++ resources/js/Authentication.vue
@@ -1,18 +1,18 @@
 <script>
-import { FRAGMENT } from '@vue/compiler-core';
-import Login from './Login.vue'
+import { FRAGMENT } from "@vue/compiler-core";
+import Login from "./Login.vue";
 
 export default {
     data() {
         return {
             loading: true,
             user: null,
-        }
+        };
     },
     methods: {
         async checkUser() {
             try {
-                const { data } = await axios.get('/api/user')
+                const { data } = await axios.get("/api/user");
                 this.loading = false;
                 this.user = data;
             } catch (e) {
@@ -22,24 +22,22 @@ export default {
         },
         async logout() {
             try {
-                await axios.post('/logout');
+                await axios.post("/logout");
             } catch (e) {
                 console.error(e);
             }
             this.checkUser();
-        }
+        },
     },
     mounted() {
         this.checkUser();
     },
-    components: { Login }
-}
+    components: { Login },
+};
 </script>
 
 <template>
-    <div v-if="loading">
-        Getting the current user...
-    </div>
+    <div v-if="loading">Getting the current user...</div>
     <div v-else>
         <div v-if="user">
             Hello {{ user.name }}
--- resources/js/Login.vue
+++ resources/js/Login.vue
@@ -2,10 +2,10 @@
 export default {
     data() {
         return {
-            email: '',
-            password: '',
+            email: "",
+            password: "",
             failed: false,
-        }
+        };
     },
     methods: {
         async login($event) {
@@ -14,30 +14,28 @@ export default {
 
             try {
                 const { email, password } = this;
-                await axios.post('/login', { email, password })
-                this.$emit('loggedIn')
+                await axios.post("/login", { email, password });
+                this.$emit("loggedIn");
             } catch (e) {
                 this.failed = true;
             }
-        }
-    }
-}
+        },
+    },
+};
 </script>
 
 <template>
     <form @submit="login">
         <div>
             <label>Email</label>
-            <input type="email" v-model="email">
+            <input type="email" v-model="email" />
         </div>
         <div>
             <label>Password</label>
-            <input type="password" v-model="password">
+            <input type="password" v-model="password" />
         </div>
         <button type="submit">Login</button>
-        <div v-if="failed">
-            Failed to log in
-        </div>
+        <div v-if="failed">Failed to log in</div>
     </form>
 </template>
 
