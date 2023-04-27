b4cf2debbf8c92f7736b4c4b7d48b9c0a00513d7
Lévai Dániel <levai.daniel.makk@gmail.com>
2023-04-27 21:47:19 +0200

    Style modify

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -15,14 +15,14 @@ onMounted(() => {
 <template>
     <div class="h-screen bg-person-stands-hallway bg-center bg-cover">
         <div class="container">
-            <h1 style="font-family:'Courier New'; font-size:4vw; color:goldenrod; font-weight: bold; text-shadow:2px 4px 4px black;">To Do App</h1>
+            <h1 style="font-family:'Courier New'; font-size:3vw; color:goldenrod; font-weight: bold; text-shadow:2px 4px 4px black;">To Do App</h1>
             <div v-if="store.initialized">
                 <Authentication>
                     <AddTask />
                     <TaskList />
                 </Authentication>
             </div>
-            <div style="font-family:'Courier New'; font-size:2vw; color:yellowgreen; font-weight: bold; text-shadow:2px 4px 4px black;" v-else>Loading...</div>
+            <div style="font-family:'Courier New'; font-size:3vw; color:yellowgreen; font-weight: bold; text-shadow:2px 4px 4px black;" v-else>Loading...</div>
         </div>
     </div>
 </template>
