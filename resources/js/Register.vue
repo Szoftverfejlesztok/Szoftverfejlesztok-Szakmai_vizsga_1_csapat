d50d5205e69c9a60fc5082983b88cfe724eb2fc9
Lévai Dániel <levai.daniel.makk@gmail.com>
2023-04-28 18:18:30 +0200

    Style modify

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -97,6 +97,14 @@ async function onSubmit($event) {
             "
                 type="submit"
          to="/login">Login</router-link>
-        <div v-if="state.failed">Failed to register</div>
+        <div
+            style="
+                font-family: 'Courier New';
+                font-size: 3vw;
+                color: crimson;
+                font-weight: bold;
+                text-shadow: 2px 4px 4px black;
+            "
+            v-if="state.failed">Failed to register</div>
     </form>
 </template>
