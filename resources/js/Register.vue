083d247ec51c384cad4de2db552ee3cf0185db05
Roland Németh <info@nemethroland.hu>
2023-04-28 22:40:45 +0200

    Improve registration

--- resources/js/Register.vue
+++ resources/js/Register.vue
@@ -31,67 +31,51 @@ async function onSubmit($event) {
 </script>
 
 <template>
-    <form @submit="onSubmit" novalidate>
-        <div
-            style="
-                font-family: 'Courier New';
-                font-size: 2vw;
-                color: lightsalmon;
-                font-weight: bold;
-                text-shadow: 2px 4px 4px black;
-            "
-            class="grid grid-cols-1 gap-4"
-        >
-            <label for="name">Name</label>
-            <input id="name" type="text" v-model="state.name" />
-            <label for="email">Email</label>
-            <input id="email" type="email" v-model="state.email" />
-            <label for="password">Password</label>
-            <input id="password" type="password" v-model="state.password" />
-            <label for="password-confirmation">Password confirmation</label>
-            <input
-                id="password-confirmation"
-                type="password"
-                v-model="state.passwordConfirmation"
-            />
+    <div class="container p-5 flex items-center">
+        <div class="box">
+            <form @submit="onSubmit" novalidate>
+                <div class="flex flex-col">
+                    <label for="name">Name</label>
+                    <input id="name" type="text" v-model="state.name" />
+                    <label for="email">Email</label>
+                    <input id="email" type="email" v-model="state.email" />
+                    <label for="password">Password</label>
+                    <input
+                        id="password"
+                        type="password"
+                        v-model="state.password"
+                    />
+                    <label for="password-confirmation"
+                        >Password confirmation</label
+                    >
+                    <input
+                        id="password-confirmation"
+                        type="password"
+                        v-model="state.passwordConfirmation"
+                    />
+                </div>
+                <div class="flex flex-col">
+                    <button class="btn-primary mb-5" type="submit">
+                        Register
+                    </button>
+                    <router-link
+                        class="btn-secondary mb-5"
+                        type="submit"
+                        to="/login"
+                        >Login</router-link
+                    >
+                    <div
+                        class="transition-all text-red-500 text-center"
+                        :class="
+                            state.failed
+                                ? ['']
+                                : ['opacity-0', 'scale-50', 'invisible']
+                        "
+                    >
+                        Failed to register
+                    </div>
+                </div>
+            </form>
         </div>
-        <button
-            class="rounded-full bg-fuchsia-800 px-10 float-left"
-            style="
-                font-family: 'Courier New';
-                font-size: 2vw;
-                color: mediumspringgreen;
-                font-weight: bold;
-                text-shadow: 2px 4px 4px black;
-            "
-            type="submit"
-        >
-            Register
-        </button>
-        <router-link
-            class="rounded-full bg-fuchsia-800 px-10 float-right"
-            style="
-                font-family: 'Courier New';
-                font-size: 2vw;
-                color: mediumspringgreen;
-                font-weight: bold;
-                text-shadow: 2px 4px 4px black;
-            "
-            type="submit"
-            to="/login"
-            >Login</router-link
-        >
-        <div
-            style="
-                font-family: 'Courier New';
-                font-size: 3vw;
-                color: crimson;
-                font-weight: bold;
-                text-shadow: 2px 4px 4px black;
-            "
-            v-if="state.failed"
-        >
-            Failed to register
-        </div>
-    </form>
+    </div>
 </template>
